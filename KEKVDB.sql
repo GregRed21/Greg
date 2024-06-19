b36e0cf3750bc2c44e00dd85ef82d3e3652662a7
GregRed21 <griphonrus@hotmail.com>
2024-06-19 08:43:18 +0300

    Add files via upload

---
+++ KEKVDB.sql
@@ -0,0 +1,21 @@
+INSERT INTO book
+VALUES 
+(1, 'The Diary if a young girl', '0199535566'), 
+(2, 'Pride and Prejudice', '9780307594006'),
+(3, 'To Kill a Mockingbird', '0446310786'),
+(4, 'The book of Gutsy woman', '1501178415'),
+(5, 'War and Peace', '1788886526');
+
+INSERT INTO publisher
+VALUES
+(1, 'Everyman`s Library', 'NY'),
+(2, 'Oxford University Press', 'NY'),
+(3, 'Grand Central Publishing', 'Washington'),
+(4, 'Simon & Sch', 'Chicago');
+
+INSERT INTO meme_cats
+VALUES
+(1, 'HAPPY_Cat', '10'),
+(2, 'RED_21', '10'),
+(3, 'Floppa_The_Cat', '8'),
+(4, 'Banana_Cat', '7')
